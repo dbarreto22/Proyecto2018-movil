8e6c6174d7b76787a548dda6a72a29bb929d464b
DiegoGutierrezFing <43502194+DiegoGutierrezFing@users.noreply.github.com>
2018-10-23 16:15:08 -0300

    Modificacion
    Se agregan los servicios rest inscripcionCarrera, inscripcionCurso e inscripcionExamen. Se modifica la firma de la operacion getAllCarreras

--- app/src/main/java/joke/hfad/com/miudelar/ApiInterface.java
+++ app/src/main/java/joke/hfad/com/miudelar/ApiInterface.java
@@ -18,9 +18,20 @@ public interface ApiInterface {
     /*@GET("/admin/usuario")
     public Call<List<DtUsuario>> getAllUsuarios();*/
 
+    /*@GET("director/carrera/")
+    public Call<List<DtCarrera>> getAllCarreras();*/
+    
     @GET("director/carrera/")
-    public Call<List<DtCarrera>> getAllCarreras();
-
+    public Call<List<DtCarrera>> getAllCarreras(@Header("Authorization") String authorization);
+    
+    @POST("estudiante/inscripcionCarrera")
+    public Call<> inscripcionCarrera(@Header("Authorization") String authorization, @Body String inscripcionCarreraDatos);
+        
+    @POST("estudiante/inscripcionCurso")
+    public Call<> inscripcionCurso(@Header("Authorization") String authorization, @Body String inscripcionCursoDatos);
+        
+    @POST("estudiante/inscripcionExamen")
+    public Call<> inscripcionExamen(@Header("Authorization") String authorization, @Body String inscripcionExamenDatos);
 
     /*@POST("/albums")
     public Call<Album> albumData(@Body Album data);*/
